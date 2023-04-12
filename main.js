379f7088db6288dcf12ac4f34d801d0a424bcd92
Mason <swartmason@gmail.com>
2023-04-11 21:05:26 -0400

    completed lab

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const heartArray = [...document.getElementsByClassName("like-glyph")];
+const modal = document.getElementById("modal");
+const modalP = document.getElementById("modal-message");
 
-// Your JavaScript code goes here!
+
+
+const callAndCatch = (e) => {
+  mimicServerCall()
+  .then(() => handleE(e))
+  .catch(error => handleError(error))
+}
+
+const handleError = (errorMessage) => {
+  modal.classList.remove("hidden")
+  const p = document.createElement("p")
+  modalP.innerText = errorMessage
+  modal.appendChild(p)
+  setTimeout(() => {
+    modal.classList.add("hidden")
+    modalP.innerText = ""
+  }, 3000)
+}
+
+const handleE = (e) => {
+  if(e.target.textContent === EMPTY_HEART){
+    e.target.classList.add("activated-heart")
+    e.target.textContent = FULL_HEART
+  } else {
+    e.target.classList.remove("activated-heart")
+    e.target.textContent = EMPTY_HEART
+  }
+
+}
+
+
+heartArray.map(heart => {
+  heart.addEventListener("click", callAndCatch)
+})
+
+
 
 
 
